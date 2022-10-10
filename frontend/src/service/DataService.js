29b06e30fe62d3d8d6ad9a9fb0a77c2044d55594
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2022-10-10 23:09:01 +0530

    Add post functions to send data to the sse server

---
+++ frontend/src/service/DataService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+class DataService{
+
+    postData(){
+      return axios.post('http://localhost:8081/publish', { 
+        query: 'FOR t IN network_traffic SORT t.traffic DESC LIMIT 5 RETURN t',
+        apiKey: localStorage.getItem('apiKey')
+      });
+
+    }  
+  }
+  
+export default new DataService();
