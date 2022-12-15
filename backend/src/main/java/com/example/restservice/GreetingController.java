b4855fe8ea02ffb686efcfb98624c7c2504f12c8
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2022-12-15 12:46:17 +0530

    Add new jar file to run the pulsar stream

--- backend/src/main/java/com/example/restservice/GreetingController.java
+++ backend/src/main/java/com/example/restservice/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.restservice;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.*;
 import java.util.concurrent.*;
@@ -10,6 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 //import com.c8db.http.HTTPEndPoint;
 //import com.c8db.http.HTTPMethod;
 //import com.c8db.http.HTTPRequest;
+import com.google.gson.Gson;
 import io.siddhi.core.SiddhiAppRuntime;
 import io.siddhi.core.SiddhiManager;
 import io.siddhi.core.query.output.callback.QueryCallback;
@@ -19,11 +21,14 @@ import io.siddhi.core.event.Event;
 import io.siddhi.extension.io.live.source.LiveSource;
 import io.siddhi.extension.map.json.sourcemapper.JsonSourceMapper;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.apache.tapestry5.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.bind.annotation.GetMapping;
 import io.micrometer.core.instrument.MeterRegistry;
 import javax.security.auth.login.CredentialException;
+
+
 
 
 @RestController
@@ -55,7 +60,7 @@ public class GreetingController {
     @CrossOrigin
     public Body publishQuery(@RequestBody Body query) {
         this.query = query.getQuery();
-        this.apiKey = query.getApiKey();
+        this.apiKey = "Tu_TZ0W2cR92-sr1j-l7ACA.newone.9pej9tihskpx2vYZaxubGW3sFCJLzxe55NRh7T0uk1JMYiRmHdiQsWh5JhRXXT6c418385";
 //        System.out.println("Data: "+query);
         System.out.println("Query: "+ this.query);
         System.out.println("API: "+ this.apiKey);
@@ -66,7 +71,7 @@ public class GreetingController {
     @CrossOrigin
     public Body publishBrowserInfo(@RequestBody Body query) {
         this.browserQuery = query.getQuery();
-        this.apiKey = query.getApiKey();
+        this.apiKey = "Tu_TZ0W2cR92-sr1j-l7ACA.newone.9pej9tihskpx2vYZaxubGW3sFCJLzxe55NRh7T0uk1JMYiRmHdiQsWh5JhRXXT6c418385";
 //        System.out.println("Data: "+query);
         System.out.println("Query: "+ this.query);
         System.out.println("API: "+ this.apiKey);
@@ -77,7 +82,7 @@ public class GreetingController {
     @CrossOrigin
     public Body setQuery(@RequestBody Body query) {
         this.dynamicQuery = query.getQuery();
-        this.apiKey = query.getApiKey();
+        this.apiKey = "Tu_TZ0W2cR92-sr1j-l7ACA.newone.9pej9tihskpx2vYZaxubGW3sFCJLzxe55NRh7T0uk1JMYiRmHdiQsWh5JhRXXT6c418385";
 //        System.out.println("Data: "+query);
         System.out.println("Query: "+ this.dynamicQuery);
         System.out.println("API: "+ this.apiKey);
@@ -101,35 +106,35 @@ public class GreetingController {
                 UUID appname = UUID.randomUUID();
                 String inStreamDefinition0 = "@App:name('"+appname.toString()+"')" +
                         "@source(type='live',sql.query='"+query+"', " +
-                        "host.name='api-varden-4f0f3c4f.paas.macrometa.io'," +
-                        "api.key = '"+apiKey+"', " +
+                        "host.name='api-peamouth-0b57f3c7.paas.macrometa.io'," +
+                        "api.key = 'Tu_TZ0W2cR92-sr1j-l7ACA.newone.9pej9tihskpx2vYZaxubGW3sFCJLzxe55NRh7T0uk1JMYiRmHdiQsWh5JhRXXT6c418385', " +
                         " @map(type='json', fail.on.missing.attribute='false') )" +
                         "define stream inputStream (id String,key String,revision String,properties String);";
 //                System.out.println("SSS: "+inStreamDefinition0);
-                    String query0 = ("@sink(type = 'log')" +
-                            "define stream OutputStream (id String,key String,revision String,properties String);" +
-                            "@info(name = 'query0') "
-                            + "from inputStream "
-                            + "select * "
-                            + "insert into outputStream;"
-                    );
-                    SiddhiAppRuntime siddhiAppRuntime0 = siddhiManager
-                            .createSiddhiAppRuntime(inStreamDefinition0 + query0);
+                String query0 = ("@sink(type = 'log')" +
+                        "define stream OutputStream (id String,key String,revision String,properties String);" +
+                        "@info(name = 'query0') "
+                        + "from inputStream "
+                        + "select * "
+                        + "insert into outputStream;"
+                );
+                SiddhiAppRuntime siddhiAppRuntime0 = siddhiManager
+                        .createSiddhiAppRuntime(inStreamDefinition0 + query0);
 
 
-                    siddhiAppRuntime0.addCallback("query0", new QueryCallback() {
-                        @Override
-                        public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
+                siddhiAppRuntime0.addCallback("query0", new QueryCallback() {
+                    @Override
+                    public void receive(long timeStamp, Event[] inEvents, Event[] removeEvents) {
 
-                            try {
-                                events.put(inEvents);
-                            } catch (InterruptedException e) {
-                                throw new RuntimeException(e);
-                            }
-
+                        try {
+                            events.put(inEvents);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
                         }
-                    });
-                    siddhiAppRuntime0.start();
+
+                    }
+                });
+                siddhiAppRuntime0.start();
 
             }
         };
@@ -144,9 +149,14 @@ public class GreetingController {
                         // we could send more events
                         while(true) {
                             Event[] edata = events.take();
-                            System.out.println(edata[0].getData()[3]);
                             list.add(edata[0].getData()[3]);
-                            System.out.println("Length: "+list.size());
+                            String json3 = edata[0].getData()[3].toString();
+                            JSONObject json1=new JSONObject(json3);
+                            long updatedTime=json1.getLong("eventTimestamp");
+                            long traffic_latency = System.currentTimeMillis() - updatedTime;
+                            System.out.println("current: "+System.currentTimeMillis()+"time: "+updatedTime+"traffic_latency: "+traffic_latency);
+//                            meterRegistry.summary("query1.latency").record(traffic_latency);'
+                            meterRegistry.timer("query1.latency").record(Duration.ofMillis(traffic_latency));
                             if(list.size() == 5) {
                                 emitter.send(list);
                                 System.out.println("Sent!");
@@ -164,7 +174,7 @@ public class GreetingController {
 
             }
         };
-        Thread t = new Thread(siddhi,"traffic");
+        Thread t = new Thread(siddhi);
         Thread tb = new Thread(sse);
         t.start();
         tb.start();
@@ -187,7 +197,7 @@ public class GreetingController {
                 }
                 String inStreamDefinition0 = "@App:name('TestSiddhiApp1')" +
                         "@source(type='live',sql.query='"+browserQuery+"', " +
-                        "host.name='api-varden-4f0f3c4f.paas.macrometa.io'," +
+                        "host.name='api-peamouth-0b57f3c7.paas.macrometa.io'," +
                         "api.key = '"+apiKey+"', " +
                         " @map(type='json', fail.on.missing.attribute='false') )" +
                         "define stream inputStream (id String,key String,revision String,properties String);";
@@ -232,6 +242,11 @@ public class GreetingController {
                             Event[] edata = events.take();
                             System.out.println(edata[0].getData()[3]);
                             list.add(edata[0].getData()[3]);
+//                            String json_browser = edata[0].getData()[3].toString();
+//                            JSONObject json2=new JSONObject(json_browser);
+//                            Long updatedTime2=json2.getLong("eventTimeStamp");
+//                            Long browser_latency = System.currentTimeMillis() - updatedTime2;
+//                            System.out.println("browser_latency:"+browser_latency);
                             if(list.size() == 5) {
                                 emitter.send(list);
                                 list.clear();
@@ -244,7 +259,7 @@ public class GreetingController {
 
             }
         };
-        Thread t = new Thread(siddhi,"browser");
+        Thread t = new Thread(siddhi);
         Thread tb = new Thread(sse);
         t.start();
         tb.start();
@@ -269,7 +284,7 @@ public class GreetingController {
                 }
                 String inStreamDefinition0 = "@App:name('TestSiddhiApp1')" +
                         "@source(type='live',sql.query='"+dynamicQuery+"', " +
-                        "host.name='api-varden-4f0f3c4f.paas.macrometa.io'," +
+                        "host.name='api-peamouth-0b57f3c7.paas.macrometa.io'," +
                         "api.key = '"+apiKey+"', " +
                         " @map(type='json', fail.on.missing.attribute='false') )" +
                         "define stream inputStream (id String,key String,revision String,properties String);";
@@ -321,20 +336,19 @@ public class GreetingController {
 
                 nonBlockingService.execute(() -> {
                     try {
-
+                        List<Object> list = new ArrayList<>(5);
                         // we could send more events
                         while(events.isEmpty()) {
-                            List<Object> list = new ArrayList<>(5);
                             Event[] edata = events.take();
                             LocalTime currentTime = java.time.LocalTime.now();
                             System.out.println("Timestamp: "+currentTime);
                             System.out.println(edata[0].getData()[3]);
                             list.add(edata[0].getData()[3]);
-//                            if(list.size() == 5) {
+                            if(list.size() == 5) {
                                 list.add(currentTime);
                                 emitter.send(list);
                                 list.clear();
-//                            }
+                            }
                         }
                     } catch (Exception ex) {
                         emitter.completeWithError(ex);
